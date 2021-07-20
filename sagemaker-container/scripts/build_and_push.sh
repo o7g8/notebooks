64241407d5dc833fc2da2988e515cc98da363338
oleg.gritsinevich@gmail.com <Oleg Grytsynevych>
2021-07-20 09:10:23 +0000

    Building a GPU container.

--- sagemaker-container/scripts/build_and_push.sh
+++ sagemaker-container/scripts/build_and_push.sh
@@ -1,14 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
+DOCKERFILE='Dockerfile'
 ACCOUNT_ID=$1
 REGION=$2
 REPO_NAME=$3
+IS_GPU=$4
 
 cd ../package/ || exit 1
 python setup.py sdist || exit 1
 cp dist/multi_model_serving-1.0.0.tar.gz ../docker/code/ || exit 1
 
-docker build --no-cache -f ../docker/Dockerfile -t $REPO_NAME ../docker
+if [[ ${IS_GPU} == 'True' ]]; then
+    DOCKERFILE='Dockerfile.gpu'
+fi
+
+echo "Building with ${DOCKERFILE}..."
+
+docker build --no-cache -f ../docker/${DOCKERFILE} -t $REPO_NAME ../docker || exit 1
 
 docker tag $REPO_NAME $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$REPO_NAME:latest
 
